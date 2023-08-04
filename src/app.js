f466ffce6b86f4a1c762abf6036ca777974ac5fd
AntonioValentin <cesar_anto30@hotmail.com>
2023-08-03 18:04:23 -0600

    Logica del Buscador 030723

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 "use strict";
 const contenedor = document.querySelector('#contenedor');
+const main = document.querySelector('#main');
+const btnSearch = document.querySelector('#input-search');
+document.addEventListener('DOMContentLoaded', loadPage);
 
-document.addEventListener('DOMContentLoaded', async () => {
-
+async function loadPage() {
     try {
         // Realizar una solicitud al endpoint externo
         const response = await fetch('https://restcountries.com/v3.1/region/europe');
-        const respuesta  = await response.json();
+        const respuesta = await response.json();
 
         respuesta.forEach((item) => {
             const createCard = document.createElement('div');
-            createCard.classList.add('bg-white','max-w-sm', 'rounded','overflow-hidden','shadow-lg','shadow-slate-500/80')
+            createCard.classList.add('bg-white', 'max-w-sm', 'rounded', 'overflow-hidden', 'shadow-lg', 'shadow-slate-500/80')
+
             const img = document.createElement('img')
             img.classList.add('w-full', 'h-48')
             img.src = item.flags.svg
+
             const contenedorText = document.createElement('div')
-            contenedorText.classList.add('px-6','py-4')
+            contenedorText.classList.add('px-6', 'py-4')
+
             const titulo = document.createElement('div')
-            titulo.classList.add('font-bold' ,'text-xl' ,'mb-2')
+            titulo.classList.add('font-bold', 'text-xl', 'mb-2')
             titulo.innerText = item.name.common;
 
             contenedor.appendChild(createCard);
@@ -25,13 +30,85 @@ document.addEventListener('DOMContentLoaded', async () => {
             createCard.appendChild(contenedorText)
             contenedorText.appendChild(titulo)
 
-
         });
-        
+
 
     } catch (error) {
 
-        console.log({ error: 'Error al obtener los datos' });
+        mostrarAlerta("Error con el Servidor")
     }
+}
+
+
+btnSearch.addEventListener('blur', async () => {
+    const valorBtn = btnSearch.value;
+
+    const peticion = await fetch(`https://restcountries.com/v3.1/name/${valorBtn}`)
+    const respuesta = await peticion.json()
+
+    limpiarHTML()
+    if (respuesta.status === 404) {
+        mostrarAlerta("Pais no Existe")
+        return
+    } else {
+
+        /* respuesta.forEach(((item) => {
+            const createCard = document.createElement('div');
+            createCard.classList.add('bg-white', 'max-w-sm', 'rounded', 'overflow-hidden', 'shadow-lg', 'shadow-slate-500/80')
+    
+            const img = document.createElement('img')
+            img.classList.add('w-full', 'h-48')
+            img.src = item.flags.svg
+    
+            const contenedorText = document.createElement('div')
+            contenedorText.classList.add('px-6', 'py-4')
+    
+            const titulo = document.createElement('div')
+            titulo.classList.add('font-bold', 'text-xl', 'mb-2')
+            titulo.innerText = item.name.common;
+    
+            contenedor.appendChild(createCard);
+            createCard.appendChild(img);
+            createCard.appendChild(contenedorText)
+            contenedorText.appendChild(titulo)
+        })) */
+    }
+    //console.log(respuesta[0].name.common);
+
 
 });
+
+function limpiarHTML() {
+
+    const contenido = document.querySelector('#main #contenedor')
+
+    while (contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild)
+    }
+
+}
+
+function mostrarAlerta(message) {
+
+    const alerta = document.createElement('div')
+    alerta.classList.add('bg-orange-100', 'border-l-4', 'border-orange-500', 'text-orange-700', 'p-4')
+    
+
+    const textoAlerta = document.createElement('p')
+    textoAlerta.classList.add('font-bold')
+    textoAlerta.textContent = "Error"
+
+    const mensajeAlerta = document.createElement('p')
+    mensajeAlerta.textContent = message
+
+    contenedor.remove()
+    alerta.appendChild(textoAlerta)
+    alerta.appendChild(mensajeAlerta)
+
+    setTimeout(() => {
+        alerta.remove()
+        loadPage()
+    }, 2000)
+
+}
+
