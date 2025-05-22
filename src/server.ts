72a7495ed280e45963eceea4a023cf464d3eafe1
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-05-21 23:38:50 -0600

    Se agrega el dontenv y el connectdb

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 // const express = require('express');
 import express from 'express';
+import 'dotenv/config';
 import router from './router.ts';
+import { connectDB } from './config/db.ts';
 const app = express();
+connectDB();
+// Leer datos de - posman y/o formulario
+app.use(express.json());
 
 app.use('/', router);
 export default app;
