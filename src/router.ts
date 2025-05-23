b113bcfcb93ce32af8ca810d3a058783cd6e94ad
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-05-22 22:33:30 -0600

    Se define la primera ruta de autenticacion para el usuario

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
+import { createAccount } from "./handlers/index.ts";
 
 const router = Router();
 
 /** Autenticacion y Registro */
-router.get("/auth/register", (req, res) => {
-    res.send("Register");
-})
+router.get("/auth/register", createAccount)
 
 export default router;
