82033ae7d827b33c0f61904d1817204da0d3c38f
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-05-24 15:07:48 -0600

    Se hashea el password y el handle a los usuarios y se guarda en la base de datos

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,16 +1,30 @@
 import User from "../models/User.ts";
+import slug from 'slug';
 import { Request, Response } from "express";
+import { hashPassword } from "../utils/auth.ts";
 
 export const createAccount = async (req : Request, res: Response) => {
     try {
-        const { email} = req.body;
+        const { email, password } = req.body;
         const userExists = await User.findOne({ email });
         if (userExists) {
             const error = new Error("User already exists");
             res.status(400).json({ msg: error.message });
             return;
         }
+
+        const handle = slug(req.body.hanle, '')
+        const handleExists = await User.findOne({ handle });
+        if (handleExists) {
+            const error = new Error("Nombre de usario no disponible");
+            res.status(400).json({ msg: error.message });
+            return;
+        }
+
         const user = new User(req.body);
+        user.password = await hashPassword(password)
+        user.handle = handle;
+
         await user.save();
         res.status(201).send("User created successfully");
     } catch (error) {
