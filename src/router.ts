1fdc99068c7208be415e96aa03065f6b9447d7a8
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-05-28 00:04:17 -0600

    Se agrega express validator en el router para registrar un usuario

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { body } from "express-validator"
 import { createAccount } from "./handlers/index.ts";
 
 const router = Router();
 
 /** Autenticacion y Registro */
-router.get("/auth/register", createAccount)
+router.post("/auth/register", 
+    body('handle').notEmpty().withMessage('El handle no puede ir vacio'),
+    body('nombre').notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('email').isEmail().withMessage('El email no es un formato valido'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('El password es muy corto, minimo 8 caracteres'),
+    createAccount
+)
 
 export default router;
