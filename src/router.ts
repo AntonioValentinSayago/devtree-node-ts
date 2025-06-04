9628948c2d46752a1a347eb0e4578f97e557e44c
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-06-03 23:46:24 -0600

    Creacion del entrypoint para el AUTH del usuario

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { body } from "express-validator"
-import { createAccount } from "./handlers/index.ts";
+import { createAccount, login } from "./handlers/index.ts";
+import { handleInputErrors } from "./middlewares/validation.ts";
 
 const router = Router();
 
@@ -12,7 +13,17 @@ router.post("/auth/register",
     body('password')
         .isLength({min: 8})
         .withMessage('El password es muy corto, minimo 8 caracteres'),
+    handleInputErrors,
     createAccount
+)
+
+router.post('/auth/login' ,
+    body('email').isEmail().withMessage('El email no es un formato valido'),
+    body('password')
+        .notEmpty()
+        .withMessage('El password es obligatorio'),
+    handleInputErrors,    
+    login
 )
 
 export default router;
