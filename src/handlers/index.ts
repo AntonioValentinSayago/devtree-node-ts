fd571bbdd80bf4e1e93071988764881ac631fac7
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-06-03 23:42:49 -0600

    Creacion de la funcion Login, para la autenticacion del usuario

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,22 +1,17 @@
 import User from "../models/User.ts";
 import { validationResult } from "express-validator";
 import slug from 'slug';
-import { Request, Response } from "express";
-import { hashPassword } from "../utils/auth.ts";
+import type { Request, Response } from 'express';
+import { checkPassword, hashPassword } from "../utils/auth.ts";
 
-export const createAccount = async (req : Request, res: Response) => {
-    // * Manejar errores
-    let errors = validationResult(req)
-    if (errors) {
-        res.status(400).json({errors: errors.array()})
-        return;
-    }
+export const createAccount = async (req: Request, res: Response) => {
+
     try {
         const { email, password } = req.body;
         const userExists = await User.findOne({ email });
         if (userExists) {
             const error = new Error("User already exists");
-            res.status(400).json({ msg: error.message });
+            res.status(409).json({ msg: error.message });
             return;
         }
 
@@ -35,6 +30,28 @@ export const createAccount = async (req : Request, res: Response) => {
         await user.save();
         res.status(201).send("User created successfully");
     } catch (error) {
-        
+        res.status(401).send('Error')
     }
+}
+
+export const login = async (req: Request, res: Response) => {
+
+    // Validar si el usuaio existe
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error("El usuario no existe");
+        res.status(401).json({ msg: error.message });
+        return;
+    }
+
+    // Comprobar el password es correcto
+    const isPasswordCorrect = await checkPassword(password, user.password)
+    if (!isPasswordCorrect) {
+        const error = new Error("El password es incorrecto");
+        res.status(401).json({ msg: error.message });
+        return;
+    }
+
+    res.send('Usuario Autenticado')
 }
