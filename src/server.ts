7a4f25194fc836a644b010c33489a34199dda22a
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-06-24 22:38:14 -0600

    Importacion de cors para su uso desde el servidor

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 // const express = require('express');
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import router from './router.ts';
 import { connectDB } from './config/db.ts';
-const app = express();
+import { corsConfig } from './config/cors.ts';
 connectDB();
+const app = express();
+
+// * Configuracion de Cors
+app.use(cors(corsConfig))
+
 // Leer datos de - posman y/o formulario
 app.use(express.json());
 
