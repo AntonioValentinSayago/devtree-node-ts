df61493100d9e6ea21224d0e54be27ef5a41511b
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-06-25 22:49:14 -0600

    Actualizacion del metodo post a su propia variable de entorno

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ErrorMessage from "../components/ErrorMessage";
 import type { RegisterForm } from "../types";
-import axios, { isAxiosError } from "axios";
+import { isAxiosError } from "axios";
+import api from "../config/axios";
 import { toast } from 'sonner';
 
 export default function RegisterView() {
@@ -21,7 +22,7 @@ export default function RegisterView() {
     const password = watch('password')
     const handleRegister = async( formData: RegisterForm ) => {
         try {
-            const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`,formData)
+            const { data } = await api.post(`/auth/register`,formData)
             toast.success(data)
             reset()
         } catch (error) {
